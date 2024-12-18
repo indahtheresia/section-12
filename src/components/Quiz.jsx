8ddf225ecfd560d376a3cfe7e271329496a8c4c5
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-18 10:52:08 +0700

    work with effect dependencies & useCallback

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer.jsx';
@@ -9,11 +9,13 @@ export default function Quiz() {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer];
     });
-  }
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
     return (
@@ -30,7 +32,7 @@ export default function Quiz() {
   return (
     <div id='quiz'>
       <div id='questions'>
-        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
           {shuffleAnswers.map((answer) => (
