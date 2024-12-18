0aaf5ee1d9f2c170e19eabbac6b1680a9a727153
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-18 23:20:23 +0700

    move logic to components that actually need it (moving state down)

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -9,18 +9,18 @@ export default function Answers({answers, selectedAnswer, answerState, onSelect}
 
   return (
     <ul id='answers'>
-          {shuffledAnswers.current.map((answer) => {
-            const isSelected = selectedAnswer === answer;
-            let cssClass = '';
-            if (answerState === 'answered' && isSelected) {
-              cssClass = 'selected';
-            }
-            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-              cssClass = answerState;
-            }
-            return <li key={answer} className='answer'>
-              <button onClick={() => {onSelect(answer)}} className={cssClass}>{answer}</button>
-            </li>;}
+        {shuffledAnswers.current.map((answer) => {
+          const isSelected = selectedAnswer === answer;
+          let cssClass = '';
+          if (answerState === 'answered' && isSelected) {
+            cssClass = 'selected';
+          }
+          if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+            cssClass = answerState;
+          }
+          return <li key={answer} className='answer'>
+            <button onClick={() => {onSelect(answer)}} className={cssClass} disabled={answerState !== ''}>{answer}</button>
+          </li>;}
         )}</ul>
   )
 }
